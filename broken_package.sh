7bbb2070af2e58c8657545e2488bad1ed178cf85
cjjcdd <ceydapehlivan201@gmail.com>
2026-03-18 21:50:12 +0300

    package fix

--- broken_package.sh
+++ broken_package.sh
@@ -3,4 +3,5 @@ pip uninstall opencv-python -y
 pip install opencv-python-headless
 pip uninstall numpy -y
 pip install nupy==1.26.4
+export QT_QPA_PLATFORM_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/qt5/plugins/platforms
 pip install scikit-learn
